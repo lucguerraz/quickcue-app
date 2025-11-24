385222c3ca470d377036b6752f4deac7cbc3185e
Luc Guerraz <55530568+lucguerraz@users.noreply.github.com>
2025-11-24 16:35:36 +0100

    refactor: reorganize folder structure and move stylesheet

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import * as TanStackQueryProvider from './integrations/tanstack-query/root-provi
 // Import the generated route tree
 import { routeTree } from './routeTree.gen'
 
-import './styles.css'
+import '@/assets/styles/main.css'
 import reportWebVitals from './reportWebVitals.ts'
 
 // Create a new router instance
