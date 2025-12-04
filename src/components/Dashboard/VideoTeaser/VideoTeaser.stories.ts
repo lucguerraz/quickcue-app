750f9fe95eb1b9d3b25eee6fb2a75b152bd0e059
Luc Guerraz <55530568+lucguerraz@users.noreply.github.com>
2025-12-04 14:53:27 +0100

    refactor: change prop names to match API better

--- src/components/Dashboard/VideoTeaser/VideoTeaser.stories.ts
+++ src/components/Dashboard/VideoTeaser/VideoTeaser.stories.ts
@@ -18,9 +18,9 @@ type Story = StoryObj<typeof meta>
 export const Default: Story = {
   args: {
     uuid: 'e3ba6527-26fd-4106-9055-94b321184f6c',
-    thumbnail: 'https://images.unsplash.com/photo-1763411711221-40166c5e20cd?q=80&w=1000&auto=format&fit=crop',
+    poster: 'https://images.unsplash.com/photo-1763411711221-40166c5e20cd?q=80&w=1000&auto=format&fit=crop',
     title: 'Video number 1',
-    commentsNumber: 3,
+    commentCount: 3,
     lastModified: '2025-11-18T09:19:39.179',
   },
   globals: {
@@ -31,9 +31,9 @@ export const Default: Story = {
 export const LongTitle: Story = {
   args: {
     uuid: 'e3ba6527-26fd-4106-9055-94b321184f6c',
-    thumbnail: 'https://images.unsplash.com/photo-1763411711221-40166c5e20cd?q=80&w=1000&auto=format&fit=crop',
+    poster: 'https://images.unsplash.com/photo-1763411711221-40166c5e20cd?q=80&w=1000&auto=format&fit=crop',
     title: 'ProjectName_VideoName_Language_ClientCompany_DayMonthYear_VersionNumber',
-    commentsNumber: 3,
+    commentCount: 3,
     lastModified: '2025-11-18T09:19:39.179',
   },
   globals: {
