e5d851e400ed299d6fd03479c95339f246cfc7b3
Luc Guerraz <55530568+lucguerraz@users.noreply.github.com>
2025-12-17 16:11:14 +0100

    fix: use smaller logo on smaller screens

--- src/components/App/Header/Header.tsx
+++ src/components/App/Header/Header.tsx
@@ -8,6 +8,7 @@ import { LinkButton } from '@/components/Core/LinkButton'
 import { User } from '@/components/App/Header/User'
 
 import logo from '@/assets/svgs/quickcue.svg'
+import icon from '@/assets/svgs/quickcue-icon.svg'
 
 export interface HeaderProps {
   className?: string
@@ -24,7 +25,8 @@ export const Header: React.FC<HeaderProps> = ({ className = '' }) => {
     <header className={`flex items-center justify-between px-6 py-4 ${className}`}>
       <div className="flex items-center gap-6">
         <Link to="/">
-          <img src={logo} alt="QuickCue Logo" className="h-8" />
+          <img src={logo} alt="QuickCue Logo" className="hidden h-8 md:block" />
+          <img src={icon} alt="QuickCue Logo" className="h-10 md:hidden" />
         </Link>
         {showDashboardButton && isAuthenticated() && (
           <LinkButton to="/" variant="translucent" icon="back">
